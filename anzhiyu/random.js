050b3e45abef368fc18fff75f5a0b2f3fd2bcbfd
Qiride <3124899838@qq.com>
2025-03-09 12:18:54 +0800

    Site updated: 2025-03-09 12:18:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["post/21ba1eea.html","post/a1286495.html","post/e9c05029.html","post/d2407e80.html","post/901ce88d.html","post/44157eb0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["post/21ba1eea.html","post/a1286495.html","post/e9c05029.html","post/d2407e80.html","post/901ce88d.html","post/44157eb0.html","post/dd1c2f0a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
